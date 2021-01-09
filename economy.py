c065832b4e65a2eeea6cd2e2487d4e9369746d31
david <blippen56@gmail.com>
2021-01-09 23:48:57 +0100

    added targetable .balance and .award command

--- economy.py
+++ economy.py
@@ -16,12 +16,17 @@ class Economy(commands.Cog):
                 json.dump(self.users, self.f)
 
     @commands.command(name='balance', invoke_without_subcommand=True)
-    async def balance(self, ctx):
-
+    async def balance(self, ctx, *, arg: str = None):
         await self.open_account(ctx.author)
+        if ctx.message.mentions:
+            mention = ctx.message.mentions[0].id
+            balance = self.users[str(mention)]["Pocket"]
+            await ctx.send(arg + " has " + str(balance) + "💰 jonbucks")
 
-        balance = self.users[str(ctx.message.author.id)]["Pocket"]
-        await ctx.send("You have " + str(balance) + "💰")
+        if arg is None:
+            print(":)")
+            balance = self.users[str(ctx.author.id)]["Pocket"]
+            await ctx.send("You have " + str(balance) + "💰 jonbucks")
 
     async def open_account(self, user):
 
@@ -33,6 +38,26 @@ class Economy(commands.Cog):
         with open("bank.json", "w") as f:
             json.dump(self.users, f)
         return True
+
+    @commands.command(name='fish', invoke_without_subcommand=True)
+    async def fish(self, message):
+        self.users[str(message.author.id)]["Pocket"] = self.users[str(message.author.id)]["Pocket"] - 10
+        await message.channel.send("fishing.. ( -10💰 )")
+
+    @commands.command(name='award', invoke_without_subcommand=True)
+    async def award(self, message, arg1, arg2):
+        if message.author.id != 540175819033542666:
+            return "Nice try"
+        if message.author.id == message.message.mentions[0].id:
+            mention = message.message.mentions[0].id
+            self.users[str(mention)]["Pocket"] = self.users[str(mention)]["Pocket"] + int(arg2)
+            await message.channel.send("you gave " + str(arg2) + "💰 jonbucks to yourself")
+        else:
+            mention = message.message.mentions[0].id
+            self.users[str(mention)]["Pocket"] = self.users[str(mention)]["Pocket"] + int(arg2)
+            await message.channel.send("you gave " + str(arg2) + "💰 jonbucks to " + str(arg1))
+
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
