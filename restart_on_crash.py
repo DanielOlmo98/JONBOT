8710ec155458b889b44d380935da9f2a2b7a29e3
dolmo <dolmo@kth.se>
2021-02-14 11:04:28 +0100

    shit

--- restart_on_crash.py
+++ restart_on_crash.py
@@ -1,15 +1,21 @@
 import subprocess
+import os
+import sys
 
 # https://stackoverflow.com/questions/44112399/automatically-restart-a-python-program-if-its-killed
 filename = 'main.py'
-while True:
-    """However, you should be careful with the '.wait()'"""
-    p = subprocess.Popen('python3 ' + filename, shell=True).wait()
+os.chdir(os.path.dirname(__file__))
+try:
+    while True:
+        """However, you should be careful with the '.wait()'"""
+        p = subprocess.Popen('python3 ' + filename, shell=True).wait()
 
-    """#if your there is an error from running 'main.py', 
-    the while loop will be repeated, 
-    otherwise the program will break from the loop"""
-    if p != 0:
-        continue
-    else:
-        break
+        """#if your there is an error from running 'main.py', 
+        the while loop will be repeated, 
+        otherwise the program will break from the loop"""
+        if p != 0:
+            continue
+        else:
+            break
+except FileNotFoundError:
+    sys.exit("File not found")
