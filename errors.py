f922f4a49342b83070f24c2181f0997bf695c0c4
iZpixl5 <eminem4300@gmail.com>
2022-05-03 19:37:08 +0200

    errroes

--- errors.py
+++ errors.py
@@ -5,8 +5,8 @@ from discord.ext import commands
 class ErrorCog(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, ChatError):
-            await ctx.send(str(error))
+        if isinstance(error.original, ChatError):
+            await ctx.send(str(error.original))
             return
 
         if isinstance(error, commands.CommandOnCooldown):
