498c45f3090c594d548b84c150f13ea841083dc5
DAVE <davee56@gmail.com>
2022-05-04 15:03:25 +0200

    new fish

--- new_fishing.py
+++ new_fishing.py
@@ -23,58 +23,83 @@ from tinydb.table import Document
 
 class NewFishingCog(commands.Cog):
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: commands.Bot, inv_filename="fish"):
         self.bot = bot
         self.db = TinyDB("test.json", indent=4)
         self.fish_table = self.db.table('fish')
         self.inventory = Inventory(self.db)
 
     def get_fishdict(self, split=True):
-        common_fish = [{'orange flopper': {'name': '<:flopper:970067116583702578>', 'size_lims': (10, 50), 'mean': 20}},
-                       {'green flopper': {'name': '<:green:971000839562993705>', 'size_lims': (10, 50), 'mean': 20}},
-                       {'blue flopper': {'name': '<:Blueflopper:971004859937611786>', 'size_lims': (10, 50),
-                                         'mean': 20}},
-                       {'light blue Small fry': {'name': '<:LightblueSmallfry:971004859153285141>',
-                                                 'size_lims': (10, 50), 'mean': 20}}
-                       ]
-        uncommon_fish = [{'yellow sweetfin': {'name': '🐠 ', 'size_lims': (10, 30), 'mean': 15}},
-                         {'drift hop flopper': {'name': '<:DrifthopFlopper:971004859270721537> ', 'size_lims': (10, 30),
-                                                'mean': 15}},
-                         {'crab': {'name': '🦀', 'size_lims': (10, 30), 'mean': 15}},
-                         {'shrimp': {'name': '🦐', 'size_lims': (10, 30), 'mean': 15}},
-                         {'lobster': {'name': '🦞', 'size_lims': (10, 30), 'mean': 15}},
-                         ]
+        common_fish = [
+            {'orange flopper': {'name': '<:flopper:970067116583702578>', 'size_lims': (10, 50), 'mean': 20}},
+            {'green flopper': {'name': '<:green:971000839562993705>', 'size_lims': (10, 50), 'mean': 20}},
+            {'blue flopper': {'name': '<:Blueflopper:971004859937611786>', 'size_lims': (10, 50),
+                              'mean': 20}},
+            {'light blue Small fry': {'name': '<:LightblueSmallfry:971004859153285141>',
+                                      'size_lims': (10, 50), 'mean': 20}},
+            {'magikarp': {'name': '<:magikar: 971342209196654642>', 'size_lims': (10, 50), 'mean': 20}},
+            {'feebas': {'name': '<:feebas: 971344403060903936> ', 'size_lims': (10, 50), 'mean': 20}},
+            {'horsea': {'name': '<:horsea:971344402612117524> ', 'size_lims': (10, 50), 'mean': 20}},
+            {'goldeen': {'name': '<:goldeen: 971344403270619156 > ', 'size_lims': (10, 50), 'mean': 20}},
+        ]
+        uncommon_fish = [
+            {'yellow sweetfin': {'name': '🐠 ', 'size_lims': (10, 30), 'mean': 15}},
+            {'drift hop flopper': {'name': '<:DrifthopFlopper:971004859270721537> ', 'size_lims': (10, 30),
+                                   'mean': 15}},
+            {'crab': {'name': '🦀', 'size_lims': (10, 30), 'mean': 15}},
+            {'shrimp': {'name': '🦐', 'size_lims': (10, 30), 'mean': 15}},
+            {'lobster': {'name': '🦞', 'size_lims': (10, 30), 'mean': 15}},
+            {'seadra': {'name': '<:seadra:971344402544996362>', 'size_lims': (50, 80), 'mean': 60}},
+            {'finneon': {'name': '<:finneon:971344403144794152>', 'size_lims': (50, 80), 'mean': 60}},
+            {'carvanha': {'name': '<:carvanha:971344402687619072>', 'size_lims': (50, 80), 'mean': 60}},
+        ]
 
-        rare_fish = [{'Spicy Fish': {'name': '<:spicy:971001500300111883>', 'size_lims': (10, 40), 'mean': 20}},
-                     {'Black and blue Shieldfish': {'name': '<:Blackandblueshieldfish:971004859648180244>',
-                                                    'size_lims': (10, 40), 'mean': 20}},
-                     {'slurp jellyfish': {'name': '<:SlurpJellyfish:971004859438481459>', 'size_lims': (10, 40),
-                                          'mean': 20}},
-                     {'dolphin': {'name': '🐬', 'size_lims': (100, 250), 'mean': 150}},
-                     {'blowfish': {'name': '🐡', 'size_lims': (20, 40), 'mean': 25}},
-                     ]
+        rare_fish = [
+            {'spicyfish': {'name': '<:spicy:971001500300111883>', 'size_lims': (10, 40), 'mean': 20}},
+            {'black and blue shieldfish': {'name': '<:Blackandblueshieldfish:971004859648180244>',
+                                           'size_lims': (10, 40), 'mean': 20}},
+            {'slurp jellyfish': {'name': '<:SlurpJellyfish:971004859438481459>', 'size_lims': (10, 40),
+                                 'mean': 20}},
+            {'dolphin': {'name': '🐬', 'size_lims': (100, 250), 'mean': 150}},
+            {'blowfish': {'name': '🐡', 'size_lims': (20, 40), 'mean': 25}},
+            {'seaking': {'name': '<:Seaking:971344403396456458>', 'size_lims': (170, 250), 'mean': 190}},
+        ]
 
-        epic_fish = [{'cockfish': {'name': 'Cock fish', 'size_lims': (1, 10)}},
-                     {'squid': {'name': '🦑', 'size_lims': (20, 40), 'mean': 25}},
-                     {'ancient Scale': {'name': '<:Ancientscale:971004858805141504>', 'size_lims': (20, 70),
-                                        'mean': 40}},
-                     {'devilfish': {'name': '<:DevilFish:971004859820171324>', 'size_lims': (20, 50), 'mean': 30}},
-                     {'blue slurpfish': {'name': '<:Blueslurpfish:971004858939351110>', 'size_lims': (20, 50),
-                                         'mean': 30}},
-                     {'stormfish': {'name': '<:Stormfish:971004859656585236>', 'size_lims': (20, 50), 'mean': 30}},
-                     ]
+        epic_fish = [
+            {'cockfish': {'name': 'Cock fish', 'size_lims': (1, 10)}},
+            {'squid': {'name': '🦑', 'size_lims': (20, 40), 'mean': 25}},
+            {'ancient Scale': {'name': '<:Ancientscale:971004858805141504>', 'size_lims': (20, 70),
+                               'mean': 40}},
+            {'devilfish': {'name': '<:DevilFish:971004859820171324>', 'size_lims': (20, 50), 'mean': 30}},
+            {'blue slurpfish': {'name': '<:Blueslurpfish:971004858939351110>', 'size_lims': (20, 50),
+                                'mean': 30}},
+            {'stormfish': {'name': '<:Stormfish:971004859656585236>', 'size_lims': (20, 50), 'mean': 30}},
+            {'kingdra': {'name': '<:kingdra:971345631325081680>', 'size_lims': (170, 210), 'mean': 180}},
+            {'milotic': {'name': '<:milotic:971344402670825523>', 'size_lims': (190, 270), 'mean': 230}},
+            {'gyarados': {'name': '<:gyarados:971344634196422686>', 'size_lims': (300, 400), 'mean': 420}},
+            {'sharpedo': {'name': '<:sharpedo:971345161118425100>', 'size_lims': (210, 290), 'mean': 230}},
+            {'nishikikoi': {'name': '<:koi:971353716739420230>', 'size_lims': (10, 30), 'mean': 15}},
+        ]
 
-        legendary_fish = [{'thermal': {'name': '<:Thermalfish:970068788991107102>', 'size_lims': (30, 70), 'mean': 40}},
-                          {'whale': {'name': '🐳', 'size_lims': (2500, 3500), 'mean': 2900}},
-                          {'octopus': {'name': '🐙', 'size_lims': (40, 90), 'mean': 50}},
-                          {'stare': {'name': ' <:stare:956548708399452211>', 'size_lims': (1, 10), 'mean': 1}},
-                          {'crocodile': {'name': '🐊', 'size_lims': (400, 700), 'mean': 500}},
-                          {'shark': {'name': '🦈', 'size_lims': (100, 250), 'mean': 150}},
-                          {'seal': {'name': '🦭', 'size_lims': (150, 300), 'mean': 200}},
-
-                          ]
-        mythical_fish = [{'midas Flopper': {'name': '', 'size_lims': (30, 70), 'mean': 40}},
-                         ]
+        legendary_fish = [
+            {'thermalfish': {'name': '<:Thermalfish:970068788991107102>', 'size_lims': (30, 70), 'mean': 40}},
+            {'whale': {'name': '🐳', 'size_lims': (2500, 3500), 'mean': 2900}},
+            {'octopus': {'name': '🐙', 'size_lims': (40, 90), 'mean': 50}},
+            {'stare': {'name': ' <:stare:956548708399452211>', 'size_lims': (1, 10), 'mean': 1}},
+            {'crocodile': {'name': '🐊', 'size_lims': (400, 700), 'mean': 500}},
+            {'shark': {'name': '🦈', 'size_lims': (100, 250), 'mean': 150}},
+            {'seal': {'name': '🦭', 'size_lims': (150, 300), 'mean': 200}},
+            {'kyogre': {'name': '<:kyogre:971344403077677127>', 'size_lims': (400, 600), 'mean': 450}},
+            {'tuna': {'name': '<:Tuna:971344403304185866>', 'size_lims': (40, 200), 'mean': 80}},
+        ]
+        mythical_fish = [
+            {'midas Flopper': {'name': '<:MidasFlopper:971004859803398154>', 'size_lims': (30, 70),
+                               'mean': 40}},
+            {'dragon': {'name': 'DORAGON 🐉', 'size_lims': (800, 1500), 'mean': 1000}},
+            {'tengu': {'name': 'japanese goburin 👺', 'size_lims': (130, 180), 'mean': 140}},
+            {'majima': {'name': 'MAJIMA NO NII SAN?? <:majimbo:971356122822877234>', 'size_lims': (130, 180),
+                        'mean': 140}},
+        ]
         if split:
             return {'common': common_fish, 'uncommon': uncommon_fish, 'rare': rare_fish, 'epic': epic_fish,
                     'legendary': legendary_fish, 'mythical': mythical_fish, }
@@ -117,7 +142,7 @@ class NewFishingCog(commands.Cog):
         def __str__(self):
             return f'{self.chat_name}'
 
-    @commands.command(name='fish')
+    @commands.command(name='fishk')
     @commands.cooldown(1, 15, commands.BucketType.user)
     async def fish(self, ctx):
         fish = self.get_fish()
